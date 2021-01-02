b0f90ef2d7cf4c9204ba35aa84ffbc83a683c9b1
Arkadyuti Sarkar <asarkar@tekion.com>
2021-01-03 01:04:05 +0530

    fixed desktop version

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -26,9 +26,24 @@ import IMG_20181231_195819 from '../../images/us/IMG_20181231_195819.jpg';
 import IMG_20191231_204426 from '../../images/us/IMG_20191231_204426.jpg';
 import IMG_20191127_002453 from '../../images/us/IMG_20191127_002453.jpg';
 
-SwiperCore.use([ Scrollbar, A11y, Autoplay, EffectFade, EffectCoverflow]);
+SwiperCore.use([Scrollbar, A11y, Autoplay, EffectFade, EffectCoverflow]);
 
 const idealRatio = 586 / 384;
+
+const mobileCheck = function() {
+  // let check = false;
+  // (function(a) {
+  //   if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+  // })(navigator.userAgent || navigator.vendor || window.opera);
+  // let check = false;
+  if (document.body.clientWidth > 767) {
+    return false
+  }
+  return true;
+};
+
+const isMobile = mobileCheck();
+console.log("isMobile:",isMobile)
 
 export default function HomePage() {
   const [galleryHeight, setGalleryHeight] = useState('588.797px');
@@ -37,7 +52,8 @@ export default function HomePage() {
   useEffect(() => {
     const width = galleryRef.current.clientWidth;
     const calculated = width * idealRatio;
-    setGalleryHeight(calculated);
+    // setGalleryHeight(calculated);
+    setGalleryHeight(isMobile ? calculated : 562);
   }, []);
 
   return (
@@ -56,7 +72,7 @@ export default function HomePage() {
         <div className="col-md-12">
           <Header/>
         </div>
-        <div className="col-md-12 no-padding">
+        <div className="col-md-12 no-padding countdown-container">
           <div>
             <div className="content">
               {/* <Snow /> */}
@@ -74,7 +90,7 @@ export default function HomePage() {
               WE ARE GETTING MARRIED (:
             </h4>
             <p className="para-content">
-              <b>“Love comes from the most unexpected places.“</b><br/>
+              <b className={'bold-text'}>“Love comes from the most unexpected places.“</b><br/>
               Please join us in our happiness as we mark the beginning of our journey into a life-long bond and
               commitment.
               {/* I'm a paragraph. Click here to add your own text and edit me. It’s
@@ -100,12 +116,12 @@ export default function HomePage() {
             <Swiper
               style={{ height: galleryHeight }}
               spaceBetween={0}
-              slidesPerView={1}
+              slidesPerView={isMobile ? 1 : 3}
               loop
-              autoplay={{
-                delay: 5000,
-                disableOnInteraction: false,
-              }}
+              // autoplay={{
+              //   delay: 5000,
+              //   disableOnInteraction: false,
+              // }}
             >
               <SwiperSlide><GridOne/></SwiperSlide>
               <SwiperSlide><GridTwo/></SwiperSlide>
@@ -121,20 +137,24 @@ export default function HomePage() {
             THE <br/>
             WEDDING
           </h1>
-          <div className="bottom-text">When</div>
-          <span className="details-text">Jan 25, 2021, 9:00 AM</span>
-
-          <div className="bottom-text">Where</div>
-          <span className="details-text">
-            Vrindavan Palace <br/> 79, Hoshangabad Rd, Shri Ram Colony <br/>{' '}
-            Bhopal (Madhya Pradesh)
+          <div className="desktop-wrapper">
+            <div className="vertical-line"/>
+            <div className="left-desktop">
+              <div className="bottom-text">When</div>
+              <span className="details-text">Jan 25, 2021, 9:00 AM</span>
+            </div>
+            <div className="right-desktop">
+              <div className="bottom-text">Where</div>
+              <span className="details-text"> Vrindavan Palace <br/> 79, Hoshangabad Rd, Shri Ram Colony <br/> Bhopal (Madhya Pradesh)
           </span>
+            </div>
+          </div>
           <a
             href="https://goo.gl/maps/NrrNFvX2qA6fTJSFA"
             target="_blank"
             className="location-cta"
           >
-            <span/>
+            <span></span>
           </a>
         </div>
         <div className="col-md-12 no-padding footer">
